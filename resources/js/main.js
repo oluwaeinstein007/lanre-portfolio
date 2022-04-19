c5d6b5ce9d845d271f458806343b953d409a85e4
Sanni, Olanrewaju <slanre26@gmail.com>
2022-04-19 21:08:02 +0100

    main js for about written

--- resources/js/main.js
+++ resources/js/main.js
@@ -3,6 +3,15 @@
         const aboutSection = document.querySelector(".about-section"),
         tabsContainer = document.querySelector('.about-tabs');
         tabsContainer.addEventListener("click", (event)=>{
-            console.log(event.target);
+            /*if event target is a 'tab-item' class and not contains 'active'*/
+            if(event.target.classList.contains("tab-item") && !event.target.classList.contains("active")){
+                const target = event.target.getAttribute("data-target");
+                //deactivate existing active 'tab-item'
+                tabsContainer.querySelector(".active").classList.remove("active","outer-shadow");
+                //deactivate existing active 'tab-content'
+                aboutSection.querySelector(".tab-content.active").classList.remove("active");
+                //activate clicked 'tab-content'
+                aboutSection.querySelector(target).classList.add("active");
+            }
         })
     })();
