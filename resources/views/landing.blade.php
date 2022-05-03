abc308dcc9dbe0bc227f6f54d9d8daafade3c16c
Sanni, Olanrewaju <slanre26@gmail.com>
2022-05-03 22:29:35 +0100

    contact section done

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -678,6 +678,76 @@
         </section>
         <!-- Testimonial section ends -->
 
+        <!-- Contact section starts -->
+        <section class="contact-section section">
+            <div class="container">
+                <div class="row">
+                    <div class="section-title">
+                        <h2 data-heading="contact">Get In Touch</h2>
+                    </div>
+                </div>
+                <div class="row">
+                    <!-- contact item start -->
+                    <div class="contact-item">
+                        <div class="contact-item-inner outer-shadow">
+                            <i class="fas fa-phone"></i>
+                            <span>Phone</span>
+                            <p>+2348057322330</p>
+                        </div>
+                    </div>
+                    <!-- contact item end -->
+                    <!-- contact item start -->
+                    <div class="contact-item">
+                        <div class="contact-item-inner outer-shadow">
+                            <i class="fas fa-envelope"></i>
+                            <span>Email</span>
+                            <p>slanre26@gmail.com</p>
+                        </div>
+                    </div>
+                    <!-- contact item end -->
+                    <!-- contact item start -->
+                    <div class="contact-item">
+                        <div class="contact-item-inner outer-shadow">
+                            <i class="fas fa-map-marker-alt"></i>
+                            <span>Address</span>
+                            <p>Jamiu Raji Street, Ikotun, Lagos</p>
+                        </div>
+                    </div>
+                    <!-- contact item end -->
+                </div>
+                <div class="row">
+                    <div class="contact-form">
+                        <form action="" method="post">
+                            <div class="row">
+                                <div class="w-50">
+                                    <div class="input-group outer-shadow hover-in-shadow">
+                                        <input type="text" name="name" placeholder="Name" class="input-control" required>
+                                    </div>
+                                    <div class="input-group outer-shadow hover-in-shadow">
+                                        <input type="email" name="email" placeholder="Email" class="input-control" required>
+                                    </div>
+                                    <div class="input-group outer-shadow hover-in-shadow">
+                                        <input type="text" name="subject" placeholder="Subject" class="input-control" required>
+                                    </div>
+                                </div>
+                                <div class="w-50">
+                                    <div class="input-group outer-shadow hover-in-shadow">
+                                        <textarea class="input-control" name="message" id="message" placeholder="Message" cols="30" rows="10"></textarea>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="submit-btn">
+                                    <button type="submit" class="btn-1 outer-shadow hover-in-shadow">Send Email</button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- Contact section ends -->
+
        <!-- Portfoli popup start -->
        <div class="pp portfolio-popup">
            <div class="pp-details">
